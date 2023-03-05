eebd0d0c936e4fe6fdc5dbcca44d4076c3e133f3
pulkeshshinde <79704977+pulkeshshinde@users.noreply.github.com>
2023-03-05 19:48:19 +0530

    Add files via upload

---
+++ Snake-Game-Java-main/src/snakegame/main.java
@@ -0,0 +1,22 @@
+package snakegame;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class main {
+    public static void main(String[] args) {
+         JFrame frame = new JFrame("Snake Game ");
+         frame.setBounds(10,10,905,700);
+         frame.setResizable(false);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+         GamePanel panel = new GamePanel();
+         panel.setBackground(Color.BLACK);
+         frame.add(panel);
+
+         frame.setVisible(true);
+
+
+
+    }
+}
